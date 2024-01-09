2ab6051778eda11b272be1ead163da8c9d13ba74
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-01-09 18:04:13 +0200

    Update proxy_auth.sh

--- src/proxy_auth.sh
+++ src/proxy_auth.sh
@@ -2,4 +2,4 @@ wget -q -O- \
 -e use_proxy=yes \
 -e http_proxy=12.13.14.15:1234  \
 --proxy-user=your_username \
---proxy-password=your_password https://ip.oxylabs.io
+--proxy-password=your_password https://ip.oxylabs.io/location
